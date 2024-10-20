3044b8503a6824ba5c751773372f43718548222e
MariaCamilaHincapie <macahlu@gmail.com>
2024-10-20 17:07:56 -0500

    Ejercicios C6 Bucles completados

--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,11 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if(Number.isInteger(x) && Number.isInteger(y)){
+    if(x > y) return x;
+    else if (x < y) return y;
+    else return x;
+  }
 }
 
 module.exports = obtenerMayor;
